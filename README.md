a2f27cb8f4f3c3297ca27eb0f306942a5db03626
Malcolm Kiano <malcolmkiano@gmail.com>
2020-03-28 04:09:14 -0400

    created app starting point

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # CSS Legends
-A game to build your knowledge of CSS, and conquer the world... kinda.
+Take the challenge and save the world from bad styling!
---
+++ src/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      CSS Legends
+    </div>
+  );
+}
+
+export default App;
